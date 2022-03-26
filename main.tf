8e7af357b60cf330e47689966cfce505a6700123
sunil kumar <sunilyadav0201@live.com>
2022-03-26 11:52:01 +0000

    added version to 1.0.1

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider   "aws"{
 }
 module "ec2" {
     source  = "app.terraform.io/sklearnit/ec2/aws"
-    version = "1.0.0"
+    version = "1.0.1"
     vpc_id=var.vpc_id  
     my_ip_with_cidr= var.my_ip_with_cidr
     public_key=var.public_key   
